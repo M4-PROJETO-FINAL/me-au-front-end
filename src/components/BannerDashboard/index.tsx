3b073df0db40edf24420076074cd40da38cb4c0b
danmatuoka <dan_mtk@hotmail.com>
2022-10-19 19:37:14 -0300

    fix: mobile

--- src/components/BannerDashboard/index.tsx
+++ src/components/BannerDashboard/index.tsx
@@ -1,85 +1,163 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import { useMediaQuery } from "react-responsive";
 
-import bg_dashboard from "../../assets/bg_dashboard.svg";
+import { Box, Container, Typography } from "@mui/material";
+
 import ImageContainer from "../../assets/ImageContainer.png";
 import petIcon from "../../assets/petIcon.svg";
 import { lightTheme } from "../../styles/themes";
-import { ImgDb } from "./styles";
+import { Button } from "../Button/style";
+import { ContainerDb, ImgDb, ImgPetIco } from "./styles";
 
 const BannerDashboard = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 425px)" });
+
   return (
-    <Container
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundImage: `url(${bg_dashboard})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        height: "50vh",
-      }}
-    >
-      <Container
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Box
+    <ContainerDb>
+      {isMobile ? (
+        <Container
           sx={{
             display: "flex",
-            flexDirection: "column",
-            gap: "20px",
-            position: "relative",
-            zIndex: 1,
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <ImgDb
-            src={petIcon}
+          <Box
             sx={{
-              width: "12%",
-              position: "absolute",
-              top: "-10px",
-              zIndex: -1,
+              display: "flex",
+              flexDirection: "column",
+              gap: isMobile ? "15px" : "20px",
+              maxWidth: "600px",
+              minWidth: "190px",
+              position: "relative",
+              zIndex: 1,
             }}
-          />
-          <Typography variant="h4" sx={{ color: lightTheme.colors.aquaLight }}>
-            Nós estamos aqui para cuidar do seu{" "}
+          >
+            <ImgPetIco src={petIcon} />
             <Typography
-              component="span"
+              className="title--db"
               variant="h4"
-              sx={{ color: lightTheme.colors.orange }}
+              sx={{ color: lightTheme.colors.aquaLight }}
             >
-              animal de estimação
+              Nós estamos aqui para cuidar do seu{" "}
+              <Typography
+                className="title--db"
+                component="span"
+                variant="h4"
+                sx={{ color: lightTheme.colors.orange }}
+              >
+                animal de estimação
+              </Typography>
             </Typography>
-          </Typography>
-          <Typography variant="h6">
-            Fazemos nossos clientes felizes oferecendo o tratamento e a estadia
-            que seu pet merece
-          </Typography>
-
-          <Box display="flex" gap="15px">
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: lightTheme.colors.orange,
-                "&:hover": lightTheme.colors.orangeLight,
-              }}
-            >
-              Reserve um horário
-            </Button>
-            <Button variant="outlined" sx={{ color: lightTheme.colors.gray1 }}>
-              Confira nossos preços
-            </Button>
+            <Typography className="subtitle--db" variant="h6">
+              Fazemos nossos clientes felizes oferecendo o tratamento e a
+              estadia que seu pet merece
+            </Typography>
+            <Box sx={{ display: "flex" }}>
+              <Box className="box--btn">
+                <Button
+                  className="btn--bg"
+                  backgroundColor="rgba(var(--orange), 1)"
+                  color="rgba(var(--white), 1)"
+                  height="2.5rem"
+                  width="100%"
+                  fontSize=".875rem"
+                  fontWeight="600"
+                  borderRadius="0.6rem"
+                >
+                  Reserve um horário
+                </Button>
+                <Button
+                  className="btn--bg border"
+                  backgroundColor="rgba(var(--white), 1)"
+                  color="rgba(var(---black), 1)"
+                  height="2.5rem"
+                  width="100%"
+                  fontSize=".875rem"
+                  fontWeight="600"
+                  borderRadius="0.6rem"
+                >
+                  Confira nossos preços
+                </Button>
+              </Box>
+              <Box className="db--mobile">
+                <ImgDb src={ImageContainer} />
+              </Box>
+            </Box>
           </Box>
-        </Box>
-        <Box>
-          <ImgDb src={ImageContainer} />
-        </Box>
-      </Container>
-    </Container>
+        </Container>
+      ) : (
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "20px",
+              maxWidth: "600px",
+              minWidth: "190px",
+              position: "relative",
+              zIndex: 1,
+            }}
+          >
+            <ImgPetIco src={petIcon} />
+            <Typography
+              className="title--db"
+              variant="h4"
+              sx={{ color: lightTheme.colors.aquaLight }}
+            >
+              Nós estamos aqui para cuidar do seu{" "}
+              <Typography
+                className="title--db"
+                component="span"
+                variant="h4"
+                sx={{ color: lightTheme.colors.orange }}
+              >
+                animal de estimação
+              </Typography>
+            </Typography>
+            <Typography className="subtitle--db" variant="h6">
+              Fazemos nossos clientes felizes oferecendo o tratamento e a
+              estadia que seu pet merece
+            </Typography>
+
+            <Box className="box--btn">
+              <Button
+                className="btn--bg"
+                backgroundColor="rgba(var(--orange), 1)"
+                color="rgba(var(--white), 1)"
+                height="2.5rem"
+                width="100%"
+                fontSize=".875rem"
+                fontWeight="600"
+                borderRadius="0.6rem"
+              >
+                Reserve um horário
+              </Button>
+              <Button
+                className="btn--bg border"
+                backgroundColor="rgba(var(--white), 1)"
+                color="rgba(var(---black), 1)"
+                height="2.5rem"
+                width="100%"
+                fontSize=".875rem"
+                fontWeight="600"
+                borderRadius="0.6rem"
+              >
+                Confira nossos preços
+              </Button>
+            </Box>
+          </Box>
+          <Box>
+            <ImgDb src={ImageContainer} />
+          </Box>
+        </Container>
+      )}
+    </ContainerDb>
   );
 };
 
--- src/components/BannerDashboard/styles.ts
+++ src/components/BannerDashboard/styles.ts
@@ -1,6 +1,79 @@
-import { styled } from "@mui/system"
+import styled from "styled-components"
+
+import bg_dashboard from "../../assets/bg_dashboard.svg";
 
 
-export const ImgDb = styled('img')({
-    width: '100%',
-})
+export const ContainerDb = styled.div`
+    height: 50vh;
+    max-width: 100vw;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url(${bg_dashboard});
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    .title--db{
+        font-weight: bold;
+        
+        @media screen and (max-width: 426px) {
+            font-size: 1.5rem;
+        } 
+    }
+
+    .subtitle--db{
+        @media screen and (max-width: 426px) {
+            display: none;
+        } 
+    }
+
+    .box--btn {
+        display: flex;
+        gap: 15px;
+
+        @media screen and (max-width: 426px) {
+            flex-direction: column;
+            justify-content: flex-end;
+            align-items: center;
+        }
+
+        .btn--bg {
+            min-width: 135px;
+            max-width: 250px;
+        }
+
+      .border {
+        border: 1px solid rgba(var(--black),1)
+      }  
+    }
+
+    .db--mobile {
+        height: 120px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`
+
+export const ImgPetIco = styled.img`
+    width: 12%;
+    position: absolute;
+    top: -10px;
+    z-index: -1;
+
+    @media screen and (max-width: 426px) {
+            width: 25%;
+        }
+`
+
+export const ImgDb = styled.img`
+    width: 100%;
+
+    @media screen and (max-width: 325px) {
+            width: 140%;
+        }
+
+
+`
