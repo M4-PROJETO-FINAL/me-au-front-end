ff1157c173fbe00c4fd4f6a6d0141691b2554b7b
HigorSkw <higor.skowronski@gmail.com>
2022-10-26 19:29:33 -0300

    feat: create addicional services Cart

--- src/components/CartModal/AddicionalServices/index.tsx
+++ src/components/CartModal/AddicionalServices/index.tsx
@@ -1,1 +1,210 @@
-export {};
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import Button from "@mui/material/Button";
+import * as yup from "yup";
+
+import { InputGlobal, InputSelectGlobal } from "../../Input";
+import { LeftSideColumn } from "../RegisterPet/styles";
+import {
+  AddServicesContainer,
+  FormServiceContainer,
+  LefSideColumn,
+  RightSideColumn,
+  TitleServices,
+  ServicesAddContainer,
+  DivSelect,
+} from "./styles";
+
+interface IFormSchemaAddServices {
+  vacinacao: string | boolean;
+  banho: number;
+  natacao: number;
+  tosaCompleta: number;
+  massagem: number;
+  racaoPremium: number;
+}
+
+const AddicionalServices = () => {
+  const { t } = useTranslation();
+
+  const ERROR_MESSAGE = t("Campo obrigatório");
+  const ERROR_INVALID_MESSAGE = t("AddServices.Campo inválido");
+
+  const ERROR_MESSAGE_NUMBER = t("AddServices.Apenas números");
+  const optionsYesAndNo = [
+    {
+      value: "yes",
+      label: t("Cadastrar pet.Sim"),
+    },
+    {
+      value: "no",
+      label: t("Cadastrar pet.Não"),
+    },
+  ];
+
+  const formSchema = yup.object().shape({
+    vacinacao: yup.string().required(ERROR_MESSAGE),
+    banho: yup
+      .number()
+      .typeError(ERROR_MESSAGE)
+      .required(ERROR_MESSAGE)
+      .min(0, ERROR_INVALID_MESSAGE)
+      .max(30, ERROR_INVALID_MESSAGE),
+    natacao: yup
+      .number()
+      .typeError(ERROR_MESSAGE)
+      .required(ERROR_MESSAGE)
+      .min(0, ERROR_INVALID_MESSAGE)
+      .max(30, ERROR_INVALID_MESSAGE),
+
+    tosaCompleta: yup
+      .number()
+      .typeError(ERROR_MESSAGE)
+      .required(ERROR_MESSAGE)
+      .min(0, ERROR_INVALID_MESSAGE)
+      .max(30, ERROR_INVALID_MESSAGE),
+    massagem: yup
+      .number()
+      .typeError(ERROR_MESSAGE)
+      .required(ERROR_MESSAGE)
+      .min(0, ERROR_INVALID_MESSAGE)
+      .max(30, ERROR_INVALID_MESSAGE),
+    racaoPremium: yup
+      .number()
+      .typeError(ERROR_MESSAGE)
+      .required(ERROR_MESSAGE)
+      .min(0, ERROR_INVALID_MESSAGE)
+      .max(30, ERROR_INVALID_MESSAGE),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormSchemaAddServices>({ resolver: yupResolver(formSchema) });
+
+  const onSubmitFunction = (data: IFormSchemaAddServices) => {
+    const newData = { ...data };
+    newData.vacinacao = data.vacinacao === "yes" ? true : false;
+    // Neste momento que será passado como um objeto para a próxima page
+    console.log(newData);
+  };
+
+  return (
+    <AddServicesContainer>
+      <div>
+        <TitleServices>
+          {t("AddServices.Escolha os serviços adicionais!")}
+        </TitleServices>
+      </div>
+
+      <FormServiceContainer onSubmit={handleSubmit(onSubmitFunction)}>
+        <ServicesAddContainer>
+          <LeftSideColumn>
+            <DivSelect>
+              <p>
+                Vacina
+                <span>(A combinar)</span>
+              </p>
+              <InputSelectGlobal
+                error={!!errors.vacinacao}
+                errorMessage={errors?.vacinacao?.message}
+                label={t("AddServices.Precisa?")}
+                registerName="vacinacao"
+                register={register}
+                options={optionsYesAndNo}
+              />
+            </DivSelect>
+
+            <DivSelect>
+              <p>
+                Banho
+                <span>(R$30,00)</span>
+              </p>
+              <InputGlobal
+                error={!!errors.banho}
+                type="number"
+                label={t("AddServices.Quantidade")}
+                errorMessage={errors?.banho?.message}
+                register={register}
+                registerName="banho"
+              />
+            </DivSelect>
+
+            <DivSelect>
+              <p>
+                Natação
+                <span>(R$50,00)</span>
+              </p>
+              <InputGlobal
+                error={!!errors.natacao}
+                type="number"
+                label={t("AddServices.Quantidade")}
+                errorMessage={errors?.natacao?.message}
+                register={register}
+                registerName="natacao"
+              />
+            </DivSelect>
+          </LeftSideColumn>
+          <RightSideColumn>
+            <DivSelect>
+              <p>
+                Tosa Completa
+                <span>(R$30,00)</span>
+              </p>
+              <InputGlobal
+                error={!!errors.tosaCompleta}
+                type="number"
+                label={t("AddServices.Quantidade")}
+                errorMessage={errors?.tosaCompleta?.message}
+                register={register}
+                registerName="tosaCompleta"
+              />
+            </DivSelect>
+            <DivSelect>
+              <p>
+                Massagem
+                <span>(R$60,00)</span>
+              </p>
+              <InputGlobal
+                error={!!errors.massagem}
+                type="number"
+                label={t("AddServices.Quantidade")}
+                errorMessage={errors?.massagem?.message}
+                register={register}
+                registerName="massagem"
+              />
+            </DivSelect>
+            <DivSelect>
+              <p>
+                Ração Premium
+                <span>(R$60,00)</span>
+              </p>
+              <InputGlobal
+                error={!!errors.racaoPremium}
+                type="number"
+                label={t("AddServices.Quantidade")}
+                errorMessage={errors?.racaoPremium?.message}
+                register={register}
+                registerName="racaoPremium"
+              />
+            </DivSelect>
+          </RightSideColumn>
+        </ServicesAddContainer>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ fontWeight: "bold", fontSize: 16 }}
+        >
+          Concluir adicionais!
+        </Button>
+      </FormServiceContainer>
+    </AddServicesContainer>
+  );
+};
+
+export default AddicionalServices;
--- src/components/CartModal/AddicionalServices/styles.ts
+++ src/components/CartModal/AddicionalServices/styles.ts
@@ -1,1 +1,110 @@
-export {};
+import styled from "styled-components";
+
+export const AddServicesContainer = styled.div`
+  overflow-y: auto;
+  height: 400px;
+  width: 100%;
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    background-color: #f5f5f5;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+    background-color: #f5f5f5;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: #fff;
+    background-image: -webkit-linear-gradient(top, #9fd4d1 50%, #65c1bc 100%);
+  }
+`;
+
+export const FormServiceContainer = styled.form`
+  width: 98%;
+  padding-bottom: 0.2rem;
+`;
+
+export const LefSideColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  @media screen and (min-width: 768px) {
+    width: 48%;
+  }
+`;
+
+export const RightSideColumn = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  flex-direction: column;
+
+  @media screen and (min-width: 768px) {
+    width: 48%;
+  }
+`;
+
+export const ServicesAddContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding-top: 0.8rem;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const TitleServices = styled.h2`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 19px;
+  text-align: center;
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+  }
+`;
+
+export const DivSelect = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: baseline;
+
+  p {
+    width: 50%;
+    display: flex;
+    gap: 7px;
+    align-items: baseline;
+    font-size: 14px;
+    font-weight: 600;
+
+    span {
+      display: none;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    p {
+      span {
+        display: flex;
+        font-size: 80%;
+        color: #121212a8;
+      }
+    }
+  }
+
+  div {
+    width: 30%;
+
+    div {
+      width: 100%;
+
+      label {
+        font-size: 80%;
+      }
+    }
+  }
+`;
