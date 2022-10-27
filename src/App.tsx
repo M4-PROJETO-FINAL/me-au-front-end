d9d9c4ae923c2364d7a54a21a87ccfaf488f36cc
danmatuoka <dan_mtk@hotmail.com>
2022-10-27 14:47:19 -0300

    feat: btn scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "react-toastify/dist/ReactToastify.css";
 
+import BtnScrollTop from "./components/BtnScrollTop";
 import Header from "./components/Header";
 import { UserContextProvider } from "./contexts/UserContext";
 import { AppRoutes } from "./routes";
@@ -20,6 +21,7 @@ function App() {
       <GoogleOAuthProvider clientId="361752817190-u5s66m46ngocih4n5drs13dmg5nht2v9.apps.googleusercontent.com">
         <UserContextProvider>
           <ThemeProvider theme={MUITheme}>
+            <BtnScrollTop />
             <Header />
             <AppRoutes />
             <ToastContainer />
