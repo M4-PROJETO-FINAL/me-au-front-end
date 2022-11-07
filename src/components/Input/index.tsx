6154c2631fb03d9f591f98a6a68ca91abd2a53bc
Guilherme Wustro <gui.wustro@gmail.com>
2022-11-07 19:47:48 -0300

    feat: password visibility add

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,6 +3,8 @@ import React, { InputHTMLAttributes } from "react";
 import { UseFormRegister } from "react-hook-form";
 import { RiErrorWarningFill } from "react-icons/ri";
 
+import { IconButton } from "@material-ui/core";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 import { InputAdornment, MenuItem, TextField } from "@mui/material";
 
 interface IInput extends InputHTMLAttributes<HTMLInputElement> {
@@ -32,6 +34,11 @@ interface ISelectGlobal extends InputHTMLAttributes<HTMLInputElement> {
   options: IOption[];
 }
 
+interface InputGlobalPassword extends IInput {
+  showPassword?: boolean;
+  handleTogglePassword?: () => void;
+}
+
 export const InputGlobal = ({
   error,
   label,
@@ -59,6 +66,51 @@ export const InputGlobal = ({
         endAdornment: error && (
           <InputAdornment position="start">
             <RiErrorWarningFill fill="#d32f2f" />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
+
+export const InputGlobalPassword = ({
+  error,
+  label,
+  register,
+  errorMessage,
+  registerName,
+  type,
+  isFullWidth,
+  maxLength,
+  fontSize,
+  showPassword,
+  handleTogglePassword,
+}: InputGlobalPassword) => {
+  return (
+    <TextField
+      error={error}
+      label={label}
+      type={showPassword ? "text" : "password"}
+      maxLength={maxLength}
+      variant="outlined"
+      id={registerName}
+      fullWidth={isFullWidth ? true : false}
+      InputLabelProps={{ style: { fontSize: `${fontSize ? fontSize : 17}px` } }}
+      helperText={error ? errorMessage : " "}
+      {...register(registerName)}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment
+            position="start"
+            style={{ display: "flex", gap: "0.5rem" }}
+          >
+            <div
+              style={{ display: "flex", cursor: "pointer" }}
+              onClick={handleTogglePassword}
+            >
+              {showPassword ? <Visibility /> : <VisibilityOff />}
+            </div>
+            {error && <RiErrorWarningFill fill="#d32f2f" />}
           </InputAdornment>
         ),
       }}
