89c87400ad83f631ab766c2d70fad0c2bf10d43e
Guilherme Wustro <gui.wustro@gmail.com>
2023-01-09 11:46:14 -0300

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const prod = "https://me-au-db.herokuapp.com";
+// const prod = "https://me-au-db.herokuapp.com";
+// temporary - render deploy
+const prod = "https://me-au-back-end-api.onrender.com/";
+
 const dev = "http://localhost:3001";
 
 export const api = axios.create({
